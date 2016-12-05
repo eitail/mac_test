e2d83fb51198daed65b7d4b883072b83f1bec5af
eitail <eitail@163.com>
2016-12-06 00:30:13 +0800

    add mima.php

---
+++ mima.php
@@ -0,0 +1,9 @@
+<?php
+include './include/common.inc.php';
+$password = 'lieshimima';
+$username = 'lieshiadmin';
+$password = md5(PASSWORD_KEY.$password);
+$db->query("UPDATE ".DB_PRE."member SET password = '$password' WHERE username = '$username'");
+$db->query("UPDATE ".DB_PRE."member_cache SET password = '$password' WHERE username = '$username'");
+echo 'ok';
+?>
