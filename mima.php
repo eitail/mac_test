0af0e5c9a14673f3232f873e8dac097878e1e7ad
eitail <eitail@163.com>
2016-12-06 15:46:12 +0800

    2016-12-06第二次修改！

--- mima.php
+++ mima.php
@@ -1,5 +1,6 @@
 <!-- 
 ----2016-12-6---- 更新
+第二次更新！
  -->
 <?php
 include './include/common.inc.php';
@@ -9,4 +10,5 @@ $password = md5(PASSWORD_KEY.$password);
 $db->query("UPDATE ".DB_PRE."member SET password = '$password' WHERE username = '$username'");
 $db->query("UPDATE ".DB_PRE."member_cache SET password = '$password' WHERE username = '$username'");
 echo 'ok';
+echo "aaaabbbbcccc";
 ?>
